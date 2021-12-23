cae06720c146f469638f3ca32f6010f8c6beaa16
Étienne Lévesque <etienne.levesque16@gmail.com>
2021-12-22 21:52:52 -0500

    Exercise 1

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components/macro';
 import { Menu, Search, User } from 'react-feather';
 
-import { QUERIES } from '../../constants';
+import { QUERIES, COLORS } from '../../constants';
 
 import MaxWidthWrapper from '../MaxWidthWrapper';
 import Logo from '../Logo';
@@ -29,7 +29,19 @@ const Header = () => {
         </Row>
       </SuperHeader>
       <MainHeader>
+        <LaptopActions>
+          <button>
+            <Search size={24} />
+          </button>
+          <button>
+            <Menu size={24} />
+          </button>
+        </LaptopActions>
         <Logo />
+        <UserBlock>
+          <SubscribeButton>Subscribe</SubscribeButton>
+          <LoginLink><em>Already a subscriber?</em></LoginLink>
+        </UserBlock>
       </MainHeader>
     </header>
   );
@@ -39,6 +51,10 @@ const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: none;
+  }
 `;
 
 const Row = styled(MaxWidthWrapper)`
@@ -59,12 +75,46 @@ const ActionGroup = styled.div`
   }
 `;
 
+const LaptopActions = styled(ActionGroup)`
+  display: none;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: flex;
+  }
+`
+
 const MainHeader = styled(MaxWidthWrapper)`
   display: flex;
   align-items: center;
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+
+  @media ${QUERIES.laptopAndUp} {
+    justify-content: space-between;
+  }
 `;
+
+const SubscribeButton = styled(Button)`
+  display: none;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: revert;
+  }
+`
+
+const LoginLink = styled.a`
+  color: ${COLORS.gray[900]};
+  text-decoration: underline;
+  position: absolute;
+  bottom: -24px;
+  left: 0;
+  width: 100%;
+  text-align: center;
+`
+
+const UserBlock = styled.div`
+  position: relative;
+`
 
 export default Header;
